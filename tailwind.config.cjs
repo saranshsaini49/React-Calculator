2d57906a7c17b0f7211bf77155d079ecf939a4eb
Saransh Saini <saranshsss99@gmail.com>
2022-12-07 04:27:09 +0530

    style:colors added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{jsx,js,tsx,ts}"],
   theme: {
+    colors: {
+      platium: "#E6E6E6",
+      "arylide-yellow": "#D6C64C",
+    },
     extend: {},
   },
   plugins: [],
