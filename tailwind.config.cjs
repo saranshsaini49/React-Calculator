37d4c1a8dbda71f4fe731ba0e5134a3f8637174b
Saransh Saini <saranshsss99@gmail.com>
2022-12-07 20:49:18 +0530

    fix:color options

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{jsx,js,tsx,ts}"],
   theme: {
-    colors: {
-      platium: "#E0E0E0",
-      orange: "#f97316",
+    extend: {
+      colors: {
+        platium: "#E0E0E0",
+        orange: "#f97316",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
