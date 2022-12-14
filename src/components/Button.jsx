54daa3a1a5288cf7d87670162d2d34b06625fdaf
Saransh Saini <saranshsss99@gmail.com>
2022-12-14 14:35:39 +0530

    chore:enter key added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,59 @@
 import React from "react";
 
+const data = [
+  "AC",
+  "+/-",
+  "%",
+  "/",
+  "7",
+  "8",
+  "9",
+  "*",
+  "4",
+  "5",
+  "6",
+  "-",
+  "1",
+  "2",
+  "3",
+  "+",
+  "0",
+  ".",
+  "=",
+];
 const Button = ({ item, display, setDisplay }) => {
+  const handleKey = (e) => {
+    const name = e.key;
+    if (name === "Enter") {
+      setDisplay(eval(display));
+    }
+    if (data.includes(name)) {
+      if (display === "" && name == ".") {
+        setDisplay("0.");
+      } else if (display !== "" && name == ".") {
+        const op = display.charAt(display.length - 1);
+        if (!(op >= "0" && op <= "9")) {
+          setDisplay(display + "0.");
+        } else {
+          setDisplay(display + ".");
+        }
+      }
+      if (display === "" && !(name >= "0" && name <= "9")) {
+      } else {
+        if (name != "AC" && name != "=" && name !== "+/-" && name != ".") {
+          const op = display.charAt(display.length - 1);
+          if (op == ".") setDisplay(display + "0" + name);
+          else setDisplay(display + name);
+        } else if (name == "=") {
+          setDisplay(eval(display));
+        } else if (name == "AC") {
+          setDisplay("");
+        } else if (name == "+/-") {
+          setDisplay(eval("-1*" + display));
+        }
+      }
+    }
+  };
   const handleClick = (e) => {
     const name = e.target.name;
     if (display === "" && name == ".") {
@@ -38,6 +91,7 @@ const Button = ({ item, display, setDisplay }) => {
             : "border border-gray-600 "
         }
         onClick={(e) => handleClick(e)}
+        onKeyPress={(e) => handleKey(e)}
       >
         {item[0]}
       </button>
@@ -46,6 +100,7 @@ const Button = ({ item, display, setDisplay }) => {
         name={item[1]}
         className="border border-gray-600 "
         onClick={(e) => handleClick(e)}
+        onKeyPress={(e) => handleKey(e)}
       >
         {item[1]}
       </button>
@@ -57,6 +112,7 @@ const Button = ({ item, display, setDisplay }) => {
             : `border border-gray-600 bg-orange-400 text-white`
         }
         onClick={(e) => handleClick(e)}
+        onKeyPress={(e) => handleKey(e)}
       >
         {item[2]}
       </button>
@@ -65,6 +121,7 @@ const Button = ({ item, display, setDisplay }) => {
           name={item[3]}
           className="border border-gray-600 bg-orange-400 text-white"
           onClick={(e) => handleClick(e)}
+          onKeyPress={(e) => handleKey(e)}
         >
           {item[3]}
         </button>
