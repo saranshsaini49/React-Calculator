4a07646d487a25203a3149a8a0601363e09cf99a
Saransh Saini <saranshsss99@gmail.com>
2022-12-17 15:31:49 +0530

    chore:moved keydown event to app to reduce re-rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,113 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 import Buttons from "./components/Buttons";
 import Display from "./components/Display";
-
+const data = [
+  "AC",
+  "+/-",
+  "%",
+  "/",
+  "7",
+  "8",
+  "9",
+  "*",
+  "4",
+  "5",
+  "6",
+  "-",
+  "1",
+  "2",
+  "3",
+  "+",
+  "0",
+  ".",
+  "=",
+];
 function App() {
   const [display, setDisplay] = useState("");
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      const name = e.key;
+      console.log(name);
+      if (name == "Enter") {
+        handlerFunction("=");
+      } else if (name == "Backspace") {
+        handlerFunction("AC");
+      } else if (data.includes(name)) {
+        handlerFunction(name);
+      }
+    });
+  }, []);
+
+  const handlerFunction = (name) => {
+    if (name == "AC") {
+      setDisplay("");
+    }
+    if (display != "") {
+      const lastChar = display.charAt(display.length - 1);
+
+      if (name >= "0" && name <= "9") {
+        setDisplay(display + name);
+      } else if (name == "=") {
+        setDisplay(eval(display) + "");
+      } else if (
+        name == "+" ||
+        name == "-" ||
+        name == "/" ||
+        name == "*" ||
+        name == "%"
+      ) {
+        if (
+          lastChar == "+" ||
+          lastChar == "-" ||
+          lastChar == "/" ||
+          lastChar == "*" ||
+          lastChar == "%"
+        ) {
+        } else if (lastChar == ".") {
+          setDisplay(display + "0" + name);
+        } else {
+          setDisplay(display + name);
+        }
+      } else if (name == "+/-") {
+        setDisplay(eval("-1*" + display) + "");
+      } else if (name == ".") {
+        if (lastChar >= "0" && lastChar <= "9") {
+          if (!isDecimal()) {
+            setDisplay(display + ".");
+          }
+        } else {
+          setDisplay(display + "0.");
+        }
+      }
+    } else {
+      if (name >= "0" && name <= "9") {
+        setDisplay(name);
+      } else if (name == ".") {
+        setDisplay("0.");
+      }
+    }
+  };
+  const isDecimal = () => {
+    let i = display.length - 1;
+
+    while (i >= 0) {
+      if (
+        display.charAt(i) == "+" ||
+        display.charAt(i) == "-" ||
+        display.charAt(i) == "/" ||
+        display.charAt(i) == "*" ||
+        display.charAt(i) == "%"
+      ) {
+        return false;
+      }
+      if (display.charAt(i) == ".") {
+        return true;
+      }
+      i--;
+    }
+    return false;
+  };
   return (
     <div className="w-screen h-screen">
       <Display display={display} />
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,112 +1,11 @@
 import React from "react";
 
-const data = [
-  "AC",
-  "+/-",
-  "%",
-  "/",
-  "7",
-  "8",
-  "9",
-  "*",
-  "4",
-  "5",
-  "6",
-  "-",
-  "1",
-  "2",
-  "3",
-  "+",
-  "0",
-  ".",
-  "=",
-];
-const Button = ({ item, display, setDisplay }) => {
-  const handleKey = (e) => {
-    const name = e.key;
-    if (name == "Enter") {
-      handlerFunction("=");
-    } else if (name == "Backspace") {
-      handlerFunction("AC");
-    } else if (data.includes(name)) {
-      handlerFunction(name);
-    }
-  };
-
-  const handlerFunction = (name) => {
-    if (name == "AC") {
-      setDisplay("");
-    }
-    if (display != "") {
-      const lastChar = display.charAt(display.length - 1);
-
-      if (name >= "0" && name <= "9") {
-        setDisplay(display + name);
-      } else if (name == "=") {
-        setDisplay(eval(display) + "");
-      } else if (
-        name == "+" ||
-        name == "-" ||
-        name == "/" ||
-        name == "*" ||
-        name == "%"
-      ) {
-        if (
-          lastChar == "+" ||
-          lastChar == "-" ||
-          lastChar == "/" ||
-          lastChar == "*" ||
-          lastChar == "%"
-        ) {
-        } else if (lastChar == ".") {
-          setDisplay(display + "0" + name);
-        } else {
-          setDisplay(display + name);
-        }
-      } else if (name == "+/-") {
-        setDisplay(eval("-1*" + display) + "");
-      } else if (name == ".") {
-        if (lastChar >= "0" && lastChar <= "9") {
-          if (!isDecimal()) {
-            setDisplay(display + ".");
-          }
-        } else {
-          setDisplay(display + "0.");
-        }
-      }
-    } else {
-      if (name >= "0" && name <= "9") {
-        setDisplay(name);
-      } else if (name == ".") {
-        setDisplay("0.");
-      }
-    }
-  };
-
+const Button = ({ item, display, setDisplay, handlerFunction }) => {
   const handleClick = (e) => {
     const name = e.target.name;
     handlerFunction(name);
   };
-  const isDecimal = () => {
-    let i = display.length - 1;
 
-    while (i >= 0) {
-      if (
-        display.charAt(i) == "+" ||
-        display.charAt(i) == "-" ||
-        display.charAt(i) == "/" ||
-        display.charAt(i) == "*" ||
-        display.charAt(i) == "%"
-      ) {
-        return false;
-      }
-      if (display.charAt(i) == ".") {
-        return true;
-      }
-      i--;
-    }
-    return false;
-  };
   return (
     <div className="w-screen grid grid-cols-4 ">
       <button
@@ -114,7 +13,6 @@ const Button = ({ item, display, setDisplay }) => {
         className={`border border-gray-600
           ${item.length == 3 ? "grid col-span-2  items-center" : ""}`}
         onClick={(e) => handleClick(e)}
-        onKeyDown={(e) => handleKey(e)}
       >
         {item[0]}
       </button>
@@ -123,7 +21,6 @@ const Button = ({ item, display, setDisplay }) => {
         name={item[1]}
         className="border border-gray-600 "
         onClick={(e) => handleClick(e)}
-        onKeyDown={(e) => handleKey(e)}
       >
         {item[1]}
       </button>
@@ -135,7 +32,6 @@ const Button = ({ item, display, setDisplay }) => {
             : `border border-gray-600 bg-orange-400 text-white`
         }`}
         onClick={(e) => handleClick(e)}
-        onKeyDown={(e) => handleKey(e)}
       >
         {item[2]}
       </button>
@@ -144,7 +40,6 @@ const Button = ({ item, display, setDisplay }) => {
           name={item[3]}
           className="border border-gray-600 bg-orange-400 text-white"
           onClick={(e) => handleClick(e)}
-          onKeyDown={(e) => handleKey(e)}
         >
           {item[3]}
         </button>
